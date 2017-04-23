08bcc8e39a033283beaa063134d4411e92b34fcf
Mark <markh4b@hotmail.com>
2017-04-23 09:35:05 +0200

    Add file choosers

---
+++ src/project/marky/java/gui/fileChooser/AbstractFileChooser.java
@@ -0,0 +1,28 @@
+package project.marky.java.gui.fileChooser;
+
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
+@SuppressWarnings("serial") // no serialization intended
+public class AbstractFileChooser extends JFileChooser
+{
+	/**
+	 * Creates a file chooser with a default directory.
+	 */
+	AbstractFileChooser()
+	{
+		super(new File(""));
+	}
+
+
+	/**
+	 * Creates a file chooser with a default directory.
+	 * 
+	 * @param defaultDirectory the directory.
+	 */
+	AbstractFileChooser(final File defaultDirectory)
+	{
+		super(defaultDirectory);
+	}
+}
